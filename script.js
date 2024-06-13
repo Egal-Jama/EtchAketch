23ba3d9b8f28b4b1de33734eb4a310352e5008f0
Egal-Jama <153658725+Egal-Jama@users.noreply.github.com>
2024-06-13 17:00:51 +0000

    Added a random block function

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-function area
-let board = document.querySelector(".board");
-let cube = board.querySelectorAll("div")
-squares.forEach((div) => div.remove());
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
+function boardArea(size) {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-for(let i = 0; i< 256; i++) {
-    let cube = document.createElement("div");
-    cube.style.backgroundColor = "Green";
-    board.insertAdjacentElement("beforeend", cube);
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement("div");
+        square.addEventListener("mouseover", colorSquare);
+        square.style.backgroundColor = "white";
+        board.insertAdjacentElement("beforeend", cube);
+    }
+
 }
-    
+
+function colorIt(){
+    if (click) {
+        if (color === "random"){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        }
+    }
+}
+boardArea(1);
+
