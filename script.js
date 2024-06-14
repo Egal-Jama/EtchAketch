a0b107c3739c5ddb7fb163f4fde8c2f3068ecf65
Egal-Jama <153658725+Egal-Jama@users.noreply.github.com>
2024-06-14 13:43:56 +0000

    Restarted everthing nothing was working for me now I will go on stack overflow for help.

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
+let click = true;
+let color = "black";
+
+
 function boardArea(size) {
-    let board = document.querySelector(".board");
-    let squares = board.querySelectorAll("div");
-    squares.forEach((div) => div.remove());
-    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
-    board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-    let amount = size * size;
-    for (let i = 0; i < amount; i++) {
-        let square = document.createElement("div");
-        square.addEventListener("mouseover", colorSquare);
-        square.style.backgroundColor = "white";
-        board.insertAdjacentElement("beforeend", cube);
-    }
-
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "green";
+    board.insertAdjacentElement("beforeend", square);
+  }
 }
-
-function colorIt(){
-    if (click) {
-        if (color === "random"){
-            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-        }
-    }
-}
-boardArea(1);
 
